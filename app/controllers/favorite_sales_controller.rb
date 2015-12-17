de7841c8724111e80cf1a009a398c3cd5f856969
Marisa Ramon <mer0522@gmail.com>
2015-12-16 21:06:49 -0600

    work on removing favorited sale

--- app/controllers/favorite_sales_controller.rb
+++ app/controllers/favorite_sales_controller.rb
@@ -3,11 +3,13 @@ class FavoriteSalesController < ApplicationController
 
   def index
     @favorites = current_user.favorite_sales
+    # @fav = Favorite.where(favorited_id: @sale.id, user_id: current_user.id)
+    @sale = Estatesale.find(params[:sale_id] || params[:id])
   end
 
-  # def show
-  #   @favorites = current_user.favorite_sales
-  # end
+  def show
+    @favorites = current_user.favorite_sales
+  end
 
   def create
     if Favorite.create(favorited: @sale, user: current_user)
@@ -18,8 +20,9 @@ class FavoriteSalesController < ApplicationController
   end
 
   def destroy
-    Favorite.where(favorited_id: @sale.id, user_id: current_user.id).first.destroy
-    redirect_to favorite_sale_path, notice: 'Sale is no longer in favorites'
+    Favorite.where(favorited_id: @sale.id, user_id: current_user.id).destroy
+
+    # redirect_to favorite_sales_path, notice: 'Sale is no longer in favorites'
   end
 
   private
